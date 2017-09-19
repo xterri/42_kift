c183f5db6f71bf8f6a71625b029bea0cc270ac4f
terri <thuynh1989@gmail.com>
2017-09-19 16:44:02 -0700

    edited bye function

--- theo_src/bye_response.c
+++ theo_src/bye_response.c
@@ -6,9 +6,11 @@
 /*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 14:37:52 by thuynh            #+#    #+#             */
-/*   Updated: 2017/09/19 14:42:54 by thuynh           ###   ########.fr       */
+/*   Updated: 2017/09/19 15:05:24 by thuynh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "server.h"
 
 char	*bye_response(char *str, int negation)
 {
