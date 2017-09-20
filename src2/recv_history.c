8ef9e699bd105d2e5d27d52e1ad03ee17ace909a
terri <thuynh1989@gmail.com>
2017-09-20 15:23:13 -0700

    readding recv_history function

---
+++ src2/recv_history.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   recv_history.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thuynh <thuynh@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/18 10:14:51 by thuynh            #+#    #+#             */
+/*   Updated: 2017/09/20 10:06:50 by thuynh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.h"
+
+void	recv_history(int cli_fd)
+{
+	int		ret;
+	int		log_fd;
+	char	*buf;
+
+	if (!(log_fd = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, 
+					S_IRUSR | S_IWUSR)))
+	{
+		perror("File cannot be opened");
+		exit(0);
+	}
+	if (!(buf = ft_strnew(255)))
+	{
+		ft_putnull("Failed to malloc.");
+		return ;
+	}
+	ft_bzero(buf, 256);
+	while ((ret = recv(cli_fd, buf, 255, 0)) != -1)
+	{
+		if (ft_strstr(buf, "-----------\n"))
+		{
+			write(log_fd, buf, ft_strlen(buf) - ft_strlen(ft_strstr(buf, "-----------\n")));
+			close(log_fd);
+			return ;
+		}
+		write(log_fd, buf, ft_strlen(buf));
+		ft_bzero(buf, 256);
+	}
+	if (ret < 0)
+	{
+		perror("Error reading socket");
+		close(log_fd);
+		exit(0);
+	}
+	close(log_fd);
+}
